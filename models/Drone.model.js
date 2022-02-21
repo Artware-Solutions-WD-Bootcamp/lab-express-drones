3a785a752700ce8336a704f34e4d7c092274267a
artware-solutions <info@artware.solutions>
2022-02-21 17:17:23 +0100

    Iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,26 @@
 // Iteration #1
+// - `name` - String (name of the drone model, like _General Atomics MQ-9 Reaper_)
+// - `propellers` - Number (amount of propellers, like _4_)
+// - `maxSpeed` - Number (meters per second for the drone's maximum speed, like _18_)
+
+//TODO 1. requerir mongoose
+const mongoose = require("mongoose")
+
+//TODO 2. crear esquema
+const droneSchema = new mongoose.Schema({
+  name: String,
+  propellers: {
+    type: Number,
+    default: 4
+  },
+  maxSpeed: {
+    type: Number,
+    max: 18
+  }
+})
+
+//TODO 3. crear modelo
+const DroneModel = mongoose.model("Drone", droneSchema)
+
+//TODO 4. exportar modelo
+module.exports = DroneModel
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,29 @@
 // Iteration #1
+//TODO 1. Define info array
+const dronesArr = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 18 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+//TODO 2. Require model
+const DroneModel = require("../models/Drone.model.js")
+
+//TODO 3. Require modeler
+const mongoose = require("mongoose")
+
+//TODO 4. Connect to DB
+mongoose.connect("mongodb://localhost/lab-express-drones")
+.then((response) => {
+  console.log("Conectado a DB")
+  //TODO 5. Insert movies
+  return DroneModel.create(dronesArr)
+})
+.then((response) => {
+  console.log("Nro. de drones insertados: ", response.length)
+  console.log("Drones insertados", response)
+  mongoose.connection.close()
+})
+.catch((err) => {
+  console.log(err)
+})
